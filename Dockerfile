74d5aa82a1357c6754beaf7554fa4ca0a4a0c5c3
martha <mctivianog@utn.edu.ec>
2026-01-14 16:43:26 -0500

    edicion nueva en la tarea

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /var/flaskapp
 COPY . .
 RUN apk update
 RUN apk add python3
-RUN pip3install -r requirements.txt
+RUN pip3 install -r requirements.txt
 EXPOSE 5000
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.router('/')
 def hola():
-    return "<h1>¡HOLA MARTHA I.A Integracion de sistemas!<h1>"
+    return "<h1>¡HOLA MARTHA I.A Integracion de sistemas y arquitectura!<h1>"
 
 if __name__== '__main__':
     app.run(host="0.0.0.0", port=5000)
